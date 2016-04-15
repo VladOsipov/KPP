616df48d81532308dfaa8f54698b00888b12e0e4
Vlad Osipov <osipov.06@mail.ru>
2016-04-16 02:33:17 +0300

    add comments

--- MenuPanel.java
+++ MenuPanel.java
@@ -1,90 +1,35 @@
 package Snake;
 
-import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JSlider;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 
 @SuppressWarnings("serial")
-public class MenuPanel extends JPanel implements ChangeListener {
+public class MenuPanel extends JPanel {
 
   private JButton btn;
   private JButton auto;
-  private JSlider speed;
-  static final int FPS_MIN = 0;
-  static final int FPS_MAX = 100;
-  static final int FPS_INIT = 10;
-  private int fps = 50;
 
   MenuPanel() {
-    btn = new JButton("Кнопочка");
+
+    btn = new JButton("Кнопочка"); // start the game
     btn.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        Snake.snake.changeTimer(fps);
-        Snake.snake.startGame();
+        Snake.snake.startGame(); // if "Кнопочка" pressed
       }
     });
-    add(btn);
+    add(btn); // add to frame
 
-    auto = new JButton("Авто");
+    auto = new JButton("Авто"); // auto mode
     auto.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        Snake.snake.startAuto();
+        Snake.snake.startAuto(); // if "Авто" pressed
       }
     });
-    add(auto);
-
-
-    speed = new JSlider(JSlider.HORIZONTAL, FPS_MIN, FPS_MAX, FPS_INIT);
-
-    speed.addChangeListener(this);
-    speed.setMajorTickSpacing(10);
-    speed.setPaintTicks(true);
-
-    add(speed);
-
-
+    add(auto); // add to frame
   }
-
-
-  public void paintComponent(Graphics g) {
-    super.paintComponent(g);
-    String str = "";
-    if (fps < 50)
-      str = "HARD";
-    if (fps >= 50 && fps < 65)
-      str = "medium";
-    if (fps >= 65 && fps < 75)
-      str = "easy";
-    if (fps >= 75)
-      str = "extra easy";
-    String string = "SPEED " + str;
-    g.setColor(Color.BLACK);
-    g.drawString(string, (int) getWidth() / 2 - string.length() / 2, 100);
-    // g.setColor(Color.red);
-    // g.fillRect(0, 0, Snake.windowX, Snake.windowY);
-  }
-
-
-  @Override
-  public void stateChanged(ChangeEvent e) {
-
-    JSlider source = (JSlider) e.getSource();
-    fps = (int) (source.getValue());
-    fps -= FPS_MAX;
-    fps = Math.abs(fps);
-    this.repaint();
-
-
-  }
-
 }
 
--- RenderPanel.java
+++ RenderPanel.java
@@ -3,7 +3,6 @@ package Snake;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
-import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
@@ -11,59 +10,36 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public class RenderPanel extends JPanel {
 
-  static int i = 0, flag = 0;
-  private ArrayList<Color> colors;
-
+  private Color YELLOW = new Color(16646040);
+  private Color GREEN = new Color(10026908);
 
   public void paintComponent(Graphics g) {
-    if (Snake.started) {
-
-
-      createColorList();
+    if (Snake.snake.started()) {
 
       super.paintComponent(g);
-      g.setColor(new Color(16646040));
-      g.fillRect(0, 0, Snake.windowX, Snake.windowY);
+      g.setColor(YELLOW);
+      g.fillRect(0, 0, Snake.windowX, Snake.windowY);// fill all the window
       Snake snake = Snake.snake;
+      g.setColor(GREEN);
 
-
-      g.setColor(new Color(10026908));
-      for (Point point : snake.snakeParts) {
+      for (Point point : snake.getSnakeParts()) { // fill all snake parts
         g.fillRect(point.x * Snake.SCALE, point.y * Snake.SCALE, Snake.SCALE, Snake.SCALE);
-        g.setColor(new Color(16646040));
+        g.setColor(YELLOW); // fill a little yellow rectangle inside the snake part
         g.fillRect(point.x * Snake.SCALE + 5, point.y * Snake.SCALE + 5, Snake.SCALE - 10,
             Snake.SCALE - 10);
-        g.setColor(new Color(10026908));
-
+        g.setColor(GREEN);
       }
 
-      g.fillRect(snake.head.x * Snake.SCALE, snake.head.y * Snake.SCALE, Snake.SCALE, Snake.SCALE);
-      g.setColor(getColor());
-
-      if (Snake.snake.tailLength < 379)
-        g.fillRect(snake.cherry.x * Snake.SCALE, snake.cherry.y * Snake.SCALE, Snake.SCALE,
+      g.fillRect(snake.getHeadX() * Snake.SCALE, snake.getHeadY() * Snake.SCALE, Snake.SCALE,
+          Snake.SCALE); // fill the head
+      g.setColor(Color.RED); // fill the cherry
+      if (Snake.snake.getTailLength() < 379)
+        g.fillRect(snake.getCherryX() * Snake.SCALE, snake.getCherryY() * Snake.SCALE, Snake.SCALE,
             Snake.SCALE);
-      String string = "Score " + snake.score + ", Length " + snake.tailLength;
+      String string = "Score " + snake.getScore() + ", Length " + snake.getTailLength();
       g.setColor(Color.BLACK);
-      g.drawString(string, (int) getWidth() / 2 - string.length() / 2, 30);
+      g.drawString(string, (int) getWidth() / 2 - string.length() / 2, 30); // print the score
+
     }
-  }
-
-  public void createColorList() {
-    colors = new ArrayList<Color>();
-    for (int i = 16750700; i < 16750831; i += 5)
-      colors.add(new Color(i));
-  }
-
-  public Color getColor() {
-    if (flag == 0)
-      i++;
-    else
-      i--;
-    if (i == colors.size() - 1)
-      flag = 1;
-    if (i == 0)
-      flag = 0;
-    return colors.get(i);
   }
 }
